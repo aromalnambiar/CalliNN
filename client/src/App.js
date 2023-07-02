dbaec19e2029361f44cdb6d896f7a14cd69043df
aromalnambiar <aromalndr@gmail.com>
2023-07-02 23:06:33 +0530

    frontend component 3 build

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import VideoPlayer from './components/VideoPlayer'
+import Notification from './components/Notification'
+
+function App() {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
